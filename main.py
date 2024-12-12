3109d7bdaed2fb3a358e0bb57b7ae5b346655521
Ding Xiaoliang <2438705414@qq.com>
2024-12-12 19:42:26 +0800

    修复了一些小bug #1 eval相关的错误02JMLR.py #2 为了远程可以使用，修改main中page_link使用相对路径 #3 PdfReader.py增加鲁棒性

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ import os
 def Index():
     st.title("论文信息查询工具 项目目录")
     # 获取pages目录下的所有文件
-    files = [f"./pages/{page}" for page in os.listdir("pages")]
-    labels = [" -> `go to`" + file.split("/")[1][:-3] for file in files]
+    filenames = [name for name in os.listdir("pages")]
+    files = [f"./pages/{name}" for name in filenames]
+    print(files)
+    # 为了可以github远程调用
+    for name,file in zip(filenames,files):
+        if not os.path.isfile(file):
+            file = f"https://github.com/MiaowFisherSpeaker/StreamlitForPapers/raw/main/pages/{name}"
+    print(files)
+    labels = [" -> `go to`" + name for name in filenames]
     for page, label in zip(files, labels):
         st.page_link(page=page, label=label)
 
@@ -25,7 +32,6 @@ def Index():
             time.sleep(0.01)
 
     st.write_stream(stream_text)
-    st.write("直播间人数超过两人开始")
 
 if __name__ == '__main__':
     Index()
